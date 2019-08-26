6263f100135f06ca2e3c203c311db25ecff0d010
pothea.chorn <pothea.chorn@PNC.LAN>
2019-08-26 18:46:00 +0700

    practice js input number

---
+++ js/practice1.js
@@ -0,0 +1,18 @@
+var num1 = 300;
+var num2 = 200;
+var sum = 0;
+var minus = 0;
+var multiply = 0;
+var devise = 0;
+
+sum = num1 + num2;
+minus = num1 - num2;
+multiply = num1 * num2;
+devise = num1 / num2; 
+
+document.getElementById("box1").innerHTML = sum;
+document.getElementById("box2").innerHTML = minus;
+document.getElementById("box3").innerHTML = multiply;
+document.getElementById("box4").innerHTML = devise;
+
+
